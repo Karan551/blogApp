12efa81ae588888d2e860e9d6c0420d98ef0ee1a
Kartik <iamkaranmishra551@gmail.com>
2024-11-09 22:22:59 +0530

    fix some name issue

--- src/conf/config.js
+++ src/conf/config.js
@@ -2,10 +2,10 @@
 export const conf = {
     appWriteURL: import.meta.env.VITE_APPWRITE_URL,
     appWriteProjectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    appWriteDataBaseID: import.meta.env.VITE_APPWRIRTE_DATABASE_ID,
-    appWriteCollectionID: import.meta.env.VITE_APPWRIRTE_COLLECTION_ID,
+    appWriteDataBaseID: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    appWriteCollectionID: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
 
-    appWriteBucketID: import.meta.env.VITE_APPWRIRTE_BUCKET_ID,
+    appWriteBucketID: import.meta.env.VITE_APPWRITE_BUCKET_ID,
 
     tinyMceApiKey: import.meta.env.VITE_TINYMCE_API_KEY
 };
